ecd30fc3437dd86d675cebe8cba01d64b97c5199
Ben Crist <ben@magicmoremagic.com>
2017-01-08 15:57:38 -0600

    build.lua syntax overhaul

--- build.lua
+++ build.lua
@@ -1,39 +1,29 @@
-module { name = 'util',
-   projects = {
-      lib {
-         src = {
-            'src/*.cpp',
-            'src/native/*.cpp',
-            'src/native/' .. toolchain .. '/*.cpp',
-            pch = 'src/pch.cpp'
-         },
-         preprocessor = {
-            'BE_UTIL_IMPL'
-         }
+module 'util' {
+   lib {
+      src {
+         'src/*.cpp',
+         'src/native/*.cpp',
+         'src/native/$(toolchain)/*.cpp'
       },
-      app { suffix = 'test',
-         icon = 'icon/bengine-test-perf.ico',
-         libs = {
-            'testing',
-            'core',
-            'core-id',
-            'util'
-         },
-         src = {
-            'test/*.cpp'
-         }
-      },
-      app { suffix = 'perf',
-         icon = 'icon/bengine-test-perf.ico',
-         libs = {
-            'testing',
-            'core',
-            'core-id',
-            'util'
-         },
-         src = {
-            'perf/*.cpp'
-         }
+      pch_src 'src/pch.cpp',
+      define 'BE_UTIL_IMPL'
+   },
+   app '-test' {
+      icon 'icon/bengine-test-perf.ico',
+      link_project {
+         'testing',
+         'core',
+         'core-id',
+         'util'
+      }
+   },
+   app '-perf' {
+      icon 'icon/bengine-test-perf.ico',
+      link_project {
+         'testing',
+         'core',
+         'core-id',
+         'util'
       }
    }
 }
