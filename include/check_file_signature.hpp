f695883abb5d6aab1ebc3a5a5fb609098c302df6
Ben Crist <ben@magicmoremagic.com>
2017-05-19 10:01:25 -0500

    _check_file_signature

--- include/check_file_signature.hpp
+++ include/check_file_signature.hpp
@@ -7,14 +7,34 @@
 namespace be::util {
 
 ///////////////////////////////////////////////////////////////////////////////
-template <typename T, typename U, std::size_t N>
+template <std::size_t O = 0, typename T = UC, typename U = T, std::size_t N = 0>
 bool file_signature_matches(const Buf<T>& buf, const U (& signature)[N]) {
-   if (buf.size() < N) {
+   if (buf.size() < O + N) {
       return false;
    }
 
+   auto ptr = buf.get() + O;
+
    for (int i = 0; i < N; ++i) {
-      if (buf[i] != signature[i]) {
+      if (ptr[i] != signature[i]) {
+         return false;
+      }
+   }
+
+   return true;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+template <typename T = UC, typename U = T, std::size_t N = 0>
+bool file_signature_matches(const Buf<T>& buf, std::size_t offset, const U(&signature)[N]) {
+   if (buf.size() < offset + N) {
+      return false;
+   }
+
+   auto ptr = buf.get() + offset;
+
+   for (int i = 0; i < N; ++i) {
+      if (ptr[i] != signature[i]) {
          return false;
       }
    }
